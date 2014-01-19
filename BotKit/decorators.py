87cef07cc902e7f590c2e7bc622676c69c693eb6
Mechazawa <Mega@IOException.at>
2014-01-19 17:41:37 +0100

    Documented the decorators

--- BotKit/decorators.py
+++ BotKit/decorators.py
@@ -3,6 +3,15 @@ _callbacks = []
 
 
 def command(name, restricted=False):
+    """
+    Decorator to add a command
+
+    @type name: str
+    @param name: The command name. This is how the command will be called
+    @type restricted: bool
+    @param restricted: if this is an admin-only command
+    @return: decorator
+    """
     global _commands
     def decorator(f):
         _commands.append({
@@ -13,6 +22,25 @@ def command(name, restricted=False):
     return decorator
 
 def handles(type, raw=False):
+    """
+    Supported types (non-raw):
+        cctp_*; (bot, user, arguments),
+        msg; (bot, channel, user, msg),
+        notice; (bot, target, user, notice),
+        invite; (bot, channel, user),
+        nick; (bot, user, nick),
+        quit; (bot, user, reason),
+        part; (bot, channel, user, reason),
+        join; (bot, channel, user)
+        kick; (bot, channel, user, target, reason)
+        mode; (bot, channel, user, target, mode)
+
+    @type type: str
+    @param type: The irc command type.
+    @type raw: bool
+    @param raw: If set to True the decorator will create a callback for a raw irc command. Callback type: parse(bot, message)
+    @return: decorator
+    """
     global _callbacks
     def decorator(f):
         _callbacks.append({
@@ -23,9 +51,27 @@ def handles(type, raw=False):
     return decorator
 
 def getcommand(name):
+    """
+    returns the requested command method
+
+    @type name: str
+    @param name:
+    @return: method
+    @rtype: dict
+    """
     global _commands
     return [c for c in _commands if c['command'] == name.lower()]
 
 def getcallback(name, raw=False):
+    """
+    returns the requested callback
+
+    @type name: str
+    @param name:
+    @type raw: bool
+    @param raw:
+    @return: method
+    @rtype: object
+    """
     global _callbacks
     return [c['method'] for c in _callbacks if c['type'] == name.lower() and c['raw'] == raw]
