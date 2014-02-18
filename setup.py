d0ff7f15e44bbc8118562d90d2585189f8296c04
Mechazawa <BasBieling@gmail.com>
2014-02-18 12:01:27 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='BotKit',
+    version='2',
+    packages=['BotKit'],
+    url='https://github.com/Mechazawa/Botkit',
+    license='GPLv2',
+    author='Mechazawa',
+    author_email='mega@ioexception.at',
+    description='A simple irc bot framework for rapid development'
+)
